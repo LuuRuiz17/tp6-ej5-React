dd433e26fe9fe38cc4e42554416097e8d829551f
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-06-11 08:43:54 -0300

    Agregué un formulario con react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormularioTareas from './components/formularioTareas';
+
 function App() {
   return (
     <>
-    <h1>Hola Mundo</h1>
+    <h1>Lista de Tareas</h1>
+    <FormularioTareas></FormularioTareas>
     </>
   )
 }
 
 export default App
+
