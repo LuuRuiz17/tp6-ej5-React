ec2b67f597f59a29fe0a76f1f94560057a792ce5
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-06-11 09:46:23 -0300

    Bajamos el footer y agregamos estilos en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FormularioTareas from './components/formularioTareas';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-    <h1>Lista de Tareas</h1>
-    <FormularioTareas></FormularioTareas>
+      <main className='container my-3'>
+        <h1 className='text-center'>Lista de Tareas</h1>
+        <FormularioTareas></FormularioTareas>
+      </main>
+      <footer className='bg-dark text-light text-center py-4'>&copy; Todos los derechos reservados</footer>
     </>
   )
 }
