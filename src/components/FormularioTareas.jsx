6aabb0943b58b3c51b22942e3b891a1967add985
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-01 21:07:48 -0300

    Agregué un condicional para que no me cree una tarea vacía apenas carga la página

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,4 +1,4 @@
-import { Form, Button, FormText} from 'react-bootstrap';
+import { Form, Button, FormText } from 'react-bootstrap';
 import ListaTareas from './listaTareas';
 import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
@@ -12,31 +12,34 @@ const formularioTareas = () => {
         formState: { errors },
         // Lo agrego para limpiar el formulario
         reset
-    } = useForm() 
+    } = useForm()
 
     const tareasLocalStorage = JSON.parse(localStorage.getItem('tareas')) || [];
-    const [tareas, setTareas] = useState([tareasLocalStorage]);
+    const [tareas, setTareas] = useState(tareasLocalStorage);
 
-    useEffect(()=>{
+    useEffect(() => {
         // Ejecuta automáticamente este código cuando suceda el ciclo de vida del componente
         // console.log("Hola desde useEffect 😍");
-        localStorage.setItem('tareas', JSON.stringify(tareas));
-    }, [tareas])
+        // Solo actualiza el localStorage si hay al menos una tarea
+        if (tareas.length > 0) {
+            localStorage.setItem('tareas', JSON.stringify(tareas));
+        }
+    }, [tareas]);
 
     // Función que se va a ejecutar si las validaciones que maneja el handlesubmit son exitosas
     const agregarTarea = (dato) => {
         console.log(dato.tarea);
         // ... sirve para iterar sobre el array y copiar cada uno de los elementos. Lo uso porque no puede manipular directamente el state. En otras palabras, estoy duplicando mi array en otro lado para poder modificarlo y después cambiarle el valor al state con setState.
         // dato.tarea es lo ques escribió el usuario en el input.
-        setTareas([...tareas, dato.tarea]); 
+        setTareas([...tareas, dato.tarea]);
         reset();
     }
 
     const borrarTarea = (nombreTarea) => {
-    // Filtrar el state tareas
-    const tareasFiltradas = tareas.filter((itemTarea) => itemTarea !== nombreTarea);
-    // Actualizamos el state
-    setTareas(tareasFiltradas);
+        // Filtrar el state tareas
+        const tareasFiltradas = tareas.filter((itemTarea) => itemTarea !== nombreTarea);
+        // Actualizamos el state
+        setTareas(tareasFiltradas);
     }
 
     return (
@@ -45,7 +48,7 @@ const formularioTareas = () => {
             <Form onSubmit={handleSubmit(agregarTarea)}>
                 <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
                     {/* register pide dos cosas: 1- Registrar con un nombre el elemento, en este caso el input. Desde ahora, cuando quiera hacer referencia al input, la librería lo reconoce como "tarea". 2- Después se agregan las validaciones entre {}. */}
-                    <Form.Control  className='bg-light' type="text" placeholder="Ingresa una tarea" {...register('tarea' , {
+                    <Form.Control className='bg-light' type="text" placeholder="Ingresa una tarea" {...register('tarea', {
                         required: 'La tarea es un campo obligatorio',
                         minLength: {
                             value: 3,
@@ -55,13 +58,13 @@ const formularioTareas = () => {
                             value: 50,
                             message: 'La tarea debe contener hasta 50 caracteres.'
                         }
-                    })}/>
+                    })} />
                     <Button variant='info ms-3'>➕</Button>
                 </Form.Group>
                 {/* Si ocurre algún error, el objeto errors va a mostrarlo aquí, si no existe lo mantiene oculto. El ?. significa que la propieda es optativa */}
                 <Form.Text className='text-danger'>{errors.tarea?.message}</Form.Text>
             </Form>
-            <ListaTareas tareas={tareas} borrarTarea = {borrarTarea}></ListaTareas>
+            <ListaTareas tareas={tareas} borrarTarea={borrarTarea}></ListaTareas>
         </div>
     );
 };
